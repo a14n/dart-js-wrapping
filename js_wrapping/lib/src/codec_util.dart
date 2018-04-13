76f4635d1a75a3fbf4d848f5ec72f4c29287e3b1
Manuel Ferreria <manuelF@users.noreply.github.com>
2018-04-13 11:53:46 +0200

    Revert "Add generic typing to identityConverter"

--- js_wrapping/lib/src/codec_util.dart
+++ js_wrapping/lib/src/codec_util.dart
@@ -6,19 +6,19 @@ library js_wrapping.src.codec_util;
 
 import 'dart:convert';
 
-class IdentityCodec<E> extends Codec<E, E> {
+class IdentityCodec extends Codec {
   const IdentityCodec();
 
   @override
-  Converter<E, E> get decoder => const _IdentityConverter();
+  Converter get decoder => const _IdentityConverter();
 
   @override
-  Converter<E, E> get encoder => const _IdentityConverter();
+  Converter get encoder => const _IdentityConverter();
 }
 
-class _IdentityConverter<E> extends Converter<E, E> {
+class _IdentityConverter extends Converter {
   const _IdentityConverter();
 
   @override
-  E convert(E input) => input;
+  convert(input) => input;
 }
